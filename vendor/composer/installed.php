6a8f9ebfd21454bc78bb88e0703b7fa17a5d9913
Denis Ziberi <denis24951@gmail.com>
2021-11-19 15:29:36 +0100

    sad

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-develop',
+    'version' => 'dev-develop',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '42735d158123f570849a6644604ee730e1024e95',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-develop',
+      'version' => 'dev-develop',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '42735d158123f570849a6644604ee730e1024e95',
+    ),
+  ),
+);
